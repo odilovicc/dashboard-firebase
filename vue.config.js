2a11106a95df7f6da8a5cd3b6ae4fbae45f5e496
Bakhtiyor <103961751+odilovicc@users.noreply.github.com>
2024-06-18 22:14:30 +0500

    Fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = {
-    plugins: [
-        new DotenvWebpackPlugin()
-    ]
+    configureWebpack: {
+        plugins: [
+            new DotenvWebpackPlugin()
+        ],
+        resolve: {
+            fallback: {
+                path: require.resolve("path-browserify"),
+                os: require.resolve("os-browserify/browser"),
+                crypto: require.resolve("crypto-browserify")
+            }
+        }
+    }
 };
